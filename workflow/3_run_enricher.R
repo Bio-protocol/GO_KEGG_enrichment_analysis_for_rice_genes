3c5743a9191676f7c373e9267aa2e64291feed2c
Yahui Li <61255999+yli085@users.noreply.github.com>
2022-03-20 22:30:04 -0700

    Update 3_run_enricher.R

--- workflow/3_run_enricher.R
+++ workflow/3_run_enricher.R
@@ -1,10 +1,10 @@
 ## R
 ## read gene lists and annotation files
 # genes of interest
-genes <- read.table("data/genes.txt", header=T)
+genes <- read.table("input/genes.txt", header=T)
 genes <- genes[[1]]
 # background genes
-bkgd <- read.table("data/bkgd.txt", header=T)
+bkgd <- read.table("input/bkgd.txt", header=T)
 bkgd <- bkgd[[1]]
 
 # TERM2GENE
@@ -25,4 +25,7 @@ go_df <- as.data.frame(go)
 write.table(go_df, "output/go_df.txt", sep="\t", row.names=FALSE, quote=FALSE)
 
 ## dot plot
-dotplot(go, showCategory=10)
+p1 <- dotplot(go, showCategory=10)
+ggsave(p1,
+       filename = "output/go_dotplot.pdf",
+       height = 12,width = 16,units = "cm") 
