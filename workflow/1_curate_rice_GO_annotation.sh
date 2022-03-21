3c59dc157f209dc3efd1375aa4528fb97717df45
Yahui Li <61255999+yli085@users.noreply.github.com>
2022-03-20 21:30:02 -0700

    Update 1_curate_rice_GO_annotation.sh

--- workflow/1_curate_rice_GO_annotation.sh
+++ workflow/1_curate_rice_GO_annotation.sh
@@ -7,10 +7,12 @@ gunzip IRGSP-1.0_representative_annotation_2021-11-11.tsv.gz
 cat IRGSP-1.0_representative_annotation_2021-11-11.tsv | perl -nale 'my @matches = ($_ =~ m/(GO:\d+)/g); foreach my $go (@matches) {print "$go\t$F[1]";} '| sort -u  > rice_rapdb_go_list.tsv
 
 # download annotation from OryzaBase
+# note that the downloaded file's name is 'download?classtag=GENE_EN_LIST',
+# but it would be 'OryzabaseGeneListEn_some_date.txt' when downloaded from a browswer
 wget https://shigen.nig.ac.jp/rice/oryzabase/gene/download?classtag=GENE_EN_LIST
 
 # extract the annotation information
-cat OryzabaseGeneListEn_20211229010053.txt | cut -f11,16 | grep -v "^\t" | perl -nle 'chomp; my @F=split /\t/, $_; my @id = ($F[0] =~ m/(Os[0-9]{2}g[0-9]{7})/g); my @go = ($F[1] =~ m/(GO:\d+)/g); if (@go>=1){ foreach my $id (@id) {  foreach my $go (@go){ print "$go\t$id" } } }' | sort -u > rice_oryzabase_go_list.tsv
+cat 'download?classtag=GENE_EN_LIST' | cut -f11,16 | grep -v "^\t" | perl -nle 'chomp; my @F=split /\t/, $_; my @id = ($F[0] =~ m/(Os[0-9]{2}g[0-9]{7})/g); my @go = ($F[1] =~ m/(GO:\d+)/g); if (@go>=1){ foreach my $id (@id) {  foreach my $go (@go){ print "$go\t$id" } } }' | sort -u > rice_oryzabase_go_list.tsv
 
 # combine the two annotation files together
 cat rice_rapdb_go_list.tsv rice_oryzabase_go_list.tsv | sort -u | perl -nple 'BEGIN {print "go_id\tgene_id";} ' > rice_combined_go_list.tsv
