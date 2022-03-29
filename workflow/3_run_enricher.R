16d5495d04b65f5108264c12e8807b2b1a46f5c3
Yahui Li <61255999+yli085@users.noreply.github.com>
2022-03-29 09:51:34 -0700

    Update 3_run_enricher.R

--- workflow/3_run_enricher.R
+++ workflow/3_run_enricher.R
@@ -1,4 +1,8 @@
-## R
+## Usage: an R script to run the univeral enrichment function from clusterProfiler using self-provided gene lists and gene annotation files. Save the output to a table, and visualize the results by a plot.
+
+library(clusterProfiler)
+library(ggplot2)
+
 ## read gene lists and annotation files
 # genes of interest
 genes <- read.table("input/genes.txt", header=T)
@@ -17,7 +21,12 @@ term2name <- read.table("data/godb_BP.txt", sep="\t", header=T, quote="", string
 go <- enricher(gene = genes, # a vector of gene id
                universe = bkgd, # background genes
                TERM2GENE = term2gene, # user input annotation of TERM TO GENE mapping 
-               TERM2NAME = term2name # user input of TERM TO NAME mapping
+               TERM2NAME = term2name, # user input of TERM TO NAME mapping
+               pvalueCutoff = 0.05, # p-value cutoff (default)
+               pAdjustMethod = "BH", # multiple testing correction method to calculate adjusted p-value (default)
+               qvalueCutoff = 0.2, # q-value cutoff (default). q value: local FDR corrected p-value.
+               minGSSize = 10, # minimal size of genes annotated for testing (default)
+               maxGSSize = 500  # maximal size of genes annotated for testing (default)
                )
 
 ## save results
