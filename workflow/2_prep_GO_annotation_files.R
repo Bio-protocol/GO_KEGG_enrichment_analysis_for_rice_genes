511d4afb61acfb8d309a4f7c0162f1399e80cacc
Yahui Li <61255999+yli085@users.noreply.github.com>
2022-03-29 22:27:49 -0700

    Update 2_prep_GO_annotation_files.R

--- workflow/2_prep_GO_annotation_files.R
+++ workflow/2_prep_GO_annotation_files.R
@@ -1,4 +1,5 @@
 ## Step 1.2 Prepare self-privided GO annoations for clusterProfiler function.
+
 ## An R script to make GO term ID to GO term name mapping tables and gene annotation (Gene ID to GO term ID) tables, 
 ## one table per one GO subcategory, i.e. BP, MF, CC.
 
@@ -18,7 +19,7 @@ godb_MF <- godb[godb$Ontology == "MF", 1:2]
 godb_CC <- godb[godb$Ontology == "CC", 1:2]
 
 # save results
-write.table(godb_BP, "cache/godb_BP.txt", sep = "\t", quote = FALSE, row.names = FALSE) 
+write.table(godb_BP, "cache/godb_BP.txt", sep = "\t", quote = FALSE, row.names = FALSE)
 write.table(godb_MF, "cache/godb_MF.txt", sep = "\t", quote = FALSE, row.names = FALSE)
 write.table(godb_CC, "cache/godb_CC.txt", sep = "\t", quote = FALSE, row.names = FALSE)
 
@@ -34,4 +35,4 @@ goid2gene_CC <- goid2gene_all %>% dplyr::filter(go_id %in% godb_CC$go_id)
 # save results
 write.table(goid2gene_BP, "cache/goid2gene_BP.txt", sep="\t", row.names=FALSE, quote=FALSE)
 write.table(goid2gene_MF, "cache/goid2gene_MF.txt", sep="\t", row.names=FALSE, quote=FALSE)
-write.table(goid2gene_CC, "cache/goid2gene_CC.txt", sep="\t", row.names=FALSE, quote=FALSE) 
+write.table(goid2gene_CC, "cache/goid2gene_CC.txt", sep="\t", row.names=FALSE, quote=FALSE)
