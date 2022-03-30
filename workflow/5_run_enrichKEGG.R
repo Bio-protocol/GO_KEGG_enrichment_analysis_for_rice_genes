1b632c6e8341e35e7032ddb1e6d3bab4208f2946
Yahui Li <61255999+yli085@users.noreply.github.com>
2022-03-29 23:01:10 -0700

    Update 5_run_enrichKEGG.R

--- workflow/5_run_enrichKEGG.R
+++ workflow/5_run_enrichKEGG.R
@@ -1,19 +1,27 @@
+## Step 3: KEGG enrichment analysis using annotations from KEGG database
+## An R script to perform KEGG enrichment analysis using an clusterProfiler function with gene annotations from the KEGG database.
+## Save the output to a table, and visualize the results by a plot.
 
-##Step 3: KEGG enrichment analysis using annotations from KEGG database
+library(clusterProfiler)
+library(data.table)
+library(ggplot2)
 
-## R
+## read gene lists
+# genes of interest
+genes <- read.table("input/genes.txt", header=T)
+genes <- genes[[1]]
+# background genes
+bkgd <- read.table("input/bkgd.txt", header=T)
+bkgd <- bkgd[[1]]
+
 ## convert the gene IDs to match with kegg database
 # read the rice gene annotaiton table (large file)
-library(data.table)
-anno <- as.data.frame(fread("cache/IRGSP-1.0_representative_annotation_2021-11-11.tsv", quote="")) #change dir
-
-
+anno <- as.data.frame(fread("cache/IRGSP-1.0_representative_annotation_2021-11-11.tsv", quote=""))
 # convert the gene ID to transcript ID (the kegg ID)
 genes_transID <- anno[match(genes, anno$Locus_ID),"Transcript_ID"]
 bkgd_transID <- anno[match(bkgd, anno$Locus_ID),"Transcript_ID"]
 
 ## run enrichKEGG
-library(clusterProfiler) #library
 kegg <- enrichKEGG(gene = genes_transID, # a vector of gene id
                    universe = bkgd_transID, # background genes
                    organism = "dosa", # kegg organism
@@ -30,8 +38,7 @@ kegg_df <- as.data.frame(kegg)
 write.table(kegg_df, "output/kegg_df.txt",  sep="\t", row.names=FALSE, quote=FALSE)
 
 ## dot plot
-library(ggplot2)
 p1 <- dotplot(kegg, showCategory=10)
 ggsave(p1,
        filename = "figures/kegg_dotplot.pdf",
-       height = 12,width = 16,units = "cm") #change dir
+       height = 12,width = 16,units = "cm") 
